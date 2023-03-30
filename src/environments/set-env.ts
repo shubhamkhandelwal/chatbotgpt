17521eada4dd130b32f92933fadc7cfa74c6d299
shubhamkhandelwal <shubham30372@gmail.com>
2023-03-30 19:56:49 +0530

    added package json

--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -3,8 +3,6 @@ const setEnv = () => {
 	const writeFile = fs.writeFile;
   // Configure Angular `environment.ts` file path
 	const targetPath = './src/environments/environment.ts';
-  // Load node modules
-	const colors = require('colors');
 	const appVersion = require('../../package.json').version;
 	require('dotenv').config({
 	  path: 'src/environments/.env'
@@ -16,13 +14,13 @@ const setEnv = () => {
 	production: true,
   };
   `;
-	console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+	console.log('The file `environment.ts` will be written with the following content: \n');
 	writeFile(targetPath, envConfigFile, (err) => {
 	  if (err) {
 		console.error(err);
 		throw err;
 	  } else {
-		console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+		console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
 	  }
 	});
   };
